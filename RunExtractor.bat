e6c3f34c7b56da6febbd710f7a3aab05367dd8fc
Travis Nesland <tnesland@Desktop.(none)>
2009-03-10 20:17:19 -0400

    Add Windows batch file to call parser.pl with GUI

---
+++ RunExtractor.bat
@@ -0,0 +1,12 @@
+
+REM Set Perl Exec Path
+path = "C:\Perl\bin\"
+
+REM Change to MedPCExtractor dir
+chdir "C:\Program Files\MedPCExtractor\src\"
+
+REM Call Perl Script
+perl parser.pl
+
+REM exit | pause (debug)
+pause
